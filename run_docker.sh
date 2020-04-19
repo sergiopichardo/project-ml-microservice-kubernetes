d4e876acc2bd8ca08575e9a3f880a591d64a6dee
Sergio Pichardo <sp88work@gmail.com>
2020-04-19 09:33:47 -0500

    update Dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,25 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+echo "----------------------------------------------------"
+echo "                  Building Image                    "
+echo "----------------------------------------------------"
+docker build -t sergiopichardo/project-ml-microservice-kubernetes . 
+printf "\n"
+printf "\n"
 
 # Step 2: 
 # List docker images
-
+# List the last created image
+echo "----------------------------------------------------"
+echo "                  Last Created Image                "
+echo "----------------------------------------------------"
+docker images | awk '{print $1}' | awk 'NR==2'
+printf "\n"
+printf "\n"
 # Step 3: 
 # Run flask app
+echo "----------------------------------------------------"
+echo "                  Running Image                     "
+echo "----------------------------------------------------"
+docker run -p 80:80 sergiopichardo/project-ml-microservice-kubernetes
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=sergiopichardo/project-ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag project-ml-microservice-kubernetes $dockerpath 
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
+
