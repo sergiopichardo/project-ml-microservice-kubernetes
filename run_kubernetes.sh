d335d90e04397cbbd724791543f7ccbe0359124f
Sergio Pichardo <sp88work@gmail.com>
2020-04-19 14:45:35 -0500

    add NodePort config yml

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,16 +3,18 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# This is your Docker ID/path (Docker Hub image)
+dockerpath=sergiopichardo/project-ml-microservice-kubernetes
+name=project-ml-microservice-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pods/$name 8000:80
