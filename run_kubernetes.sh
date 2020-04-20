fbc6baab5ddea65b3e1812be8c18c644f13cdd62
Sergio Pichardo <sp88work@gmail.com>
2020-04-20 13:55:46 -0500

    fix error in k8s deployment

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -10,30 +10,26 @@ name=project-ml-microservice-kubernetes
 echo "----------------------------------------------------"
 echo "      Run Docker Hub Container with Kubernetes      "
 echo "----------------------------------------------------"
-# Create a deployment 
+docker login
 kubectl create deployment $name --image=$dockerpath
-# List deployments
-kubectl get deployments
-# NOTE: kubectl run --generator=deployment/apps.v1 is DEPRECATED and will be removed in a future version. 
-#   Use kubectl run --generator=run-pod/v1
 kubectl run --generator=run-pod/v1 $name --image=$dockerpath --port=80
-
-# wait until pods are running to execute next command
-sleep 3 & 
-wait
 printf "\n"
 printf "\n"
-
 
 # Step 3:
 # List kubernetes pods
 echo "----------------------------------------------------"
 echo "                List Kubernetes Pods                "
 echo "----------------------------------------------------"
-kubectl get pods
+kubectl get deployments
+kubectl get pods 
 printf "\n"
 printf "\n"
 
+# wait until pods are running to execute next command
+echo "Waiting 10 seconds for pod to be ready..."
+sleep 10 & 
+wait
 
 # Step 4:
 # Forward the container port to a host
