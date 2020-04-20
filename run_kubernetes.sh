6a96a1f96f30fc9dff7bbab5b929caf34d4e1c19
Sergio Pichardo <sp88work@gmail.com>
2020-04-20 08:06:40 -0500

    update ./run_kubernetes.sh file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path (Docker Hub image)
-docker stack deploy --namespace my-app --compose-file ./docker-compose.yml
-dockerpath=sergiopichardo/project-ml-microservice-kubernetes
 name=project-ml-microservice-kubernetes
+dockerpath=sergiopichardo/project-ml-microservice-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run --image=$dockerpath --port=80
+# NOTE: kubectl run --generator=deployment/apps.v1 is DEPRECATED and will be removed in a future version. 
+#   Use kubectl run --generator=run-pod/v1
+docker apply -f $dockerpath
+kubectl run --generator=run-pod/v1 $name --image=$dockerpath --port=80
+
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
